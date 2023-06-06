edcc05eec9e611b0ea5ff406efeba3fb05acc96b
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2023-06-06 10:36:53 -0300

    continuando o código da calculadora, soma funcionando

--- calculadora.asm
+++ calculadora.asm
@@ -1,5 +1,12 @@
 .data 
-	menu_msg: .asciiz "Esse será o texto do menu"
+  welcome_msg: .asciiz "Bem víndo a calculadora em Assembly MIPS\n"
+	menu_msg: .asciiz "Escolha a operação que deseja realizar:\nA - Adição\nS - Subtração\nM - Multiplicação\nD - Divisão\nP - Potenciação\nF - Fechar\nDigite o número da operação: "
+
+  first_num_msg: .asciiz "Digite o primeiro número: "
+  second_num_msg: .asciiz "Digite o segundo número: "
+
+  blank_line: .asciiz "\n\n"
+
   response_msg: .asciiz "O resultado da operação é: "
   exit_msg: .asciiz "Obrigado por usar a calculadora"
 
@@ -7,16 +14,66 @@
 .globl main
 
 main:
+  li $v0, 4
+  la $a0, welcome_msg
+  syscall
+
   j while
 
 while:
-  # Aqui será o laço que roda o programa
-  # O usuário escolhe a operação que deseja realizar
+  li $v0, 4
+  la $a0, menu_msg
+  syscall
+
+  li $v0, 12
+  syscall
+
+  move $t0, $v0
+
+  # Se o usuário escolher a opção F, o programa sai
+
+  li $t3, 'F'
+  beq $t0, $t3, exit
+
+  li $t3, 'f'
+  beq $t0, $t3, exit
+
+  # Se o usuário escolher uma opção inválida, o programa volta para o começo do while
+
+  # blt $t0, 1, while
+  # bgt $t0, 6, while
+
   # O usuário escolhe os dois números que deseja realizar a operação
+
+  li $v0, 4
+  la $a0, first_num_msg
+  syscall
+
+  li $v0, 5
+  syscall
+
+  move $t1, $v0
+
+  li $v0, 4
+  la $a0, second_num_msg
+  syscall
+
+  li $v0, 5
+  syscall
+
+  move $t2, $v0
+
+  li $t3, 'A'
+  beq $t0, $t3, adicao
+
+  li $t3, 'a'
+  beq $t0, $t3, adicao
+
   # O programa chama a função da operação escolhida
 
-soma:
-  # Realiza a soma dos dois números
+adicao:
+  add $t4, $t1, $t2
+
   j print_result
 
 subtracao:
@@ -41,7 +98,12 @@ print_result:
   la $a0, response_msg
   syscall
 
+  move $a0, $t4
   li $v0, 1
+  syscall
+
+  li $v0, 4
+  la $a0, blank_line
   syscall
 
   j while
